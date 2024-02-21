c8f82bc239baa106fd1b1aff649f37e9070873a6
christianstolev <152278862+christianstolev@users.noreply.github.com>
2024-02-21 06:58:09 +0100

    Responsivitet och animationer

--- js/landing.js
+++ js/landing.js
@@ -1,3 +1,4 @@
+
 // Req: JQUERY
 $(document).ready(function() {
     var containers = $('.container');
@@ -89,4 +90,69 @@ $(document).ready(function() {
             });
         })
     }
+    
+    /* Store the element in el */
+    let els = document.getElementsByClassName('platform')
+    for (let el of els) {
+        const height = el.clientHeight
+        const width = el.clientWidth
+
+        /*
+        * Add a listener for mousemove event
+        * Which will trigger function 'handleMove'
+        * On mousemove
+        */
+        el.addEventListener('mousemove', handleMove)
+
+        /* Define function a */
+        function handleMove(e) {
+            el.parentElement.style.zIndex = 100
+            /*
+                * Get position of mouse cursor
+                * With respect to the element
+                * On mouseover
+                */
+            /* Store the x position */
+            const xVal = e.layerX
+            /* Store the y position */
+            const yVal = e.layerY
+            
+            /*
+                * Calculate rotation valuee along the Y-axis
+                * Here the multiplier 20 is to
+                * Control the rotation
+                * You can change the value and see the results
+                */
+            const yRotation = 30 * ((xVal - width / 2) / width)
+            
+            /* Calculate the rotation along the X-axis */
+            const xRotation = -30 * ((yVal - height / 2) / height)
+            
+            /* Generate string for CSS transform property */
+            const string = 'perspective(500px) scale(1.2) rotateX(' + xRotation + 'deg) rotateY(' + yRotation + 'deg)'
+            
+            /* Apply the calculated transformation */
+            el.style.transform = string
+        }
+
+        /* Add listener for mouseout event, remove the rotation */
+        el.addEventListener('mouseout', function() {
+        el.style.transform = 'perspective(500px) scale(1) rotateX(0) rotateY(0)'
+        el.parentElement.style.zIndex = 0
+        })
+
+        /* Add listener for mousedown event, to simulate click */
+        el.addEventListener('mousedown', function() {
+        el.style.transform = 'perspective(500px) scale(0.9) rotateX(0) rotateY(0)'
+        })
+
+        /* Add listener for mouseup, simulate release of mouse click */
+        el.addEventListener('mouseup', function() {
+        el.style.transform = 'perspective(500px) scale(1.1) rotateX(0) rotateY(0)'
+        })
+    }
+    /* Get the height and width of the element */
+    
 });
+
+
